9bf0432c73ddcda6f62177a1d37798fbf92750de
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2018-03-24 21:23:06 +0200

    change email function

--- src/Controller/Admin/UserController.php
+++ src/Controller/Admin/UserController.php
@@ -5,15 +5,18 @@ namespace App\Controller\Admin;
 use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 
 /**
  * Class UserController
  */
 class UserController extends Controller
 {
-
     /**
      * @Route("/profile", name="user_profile")
+     * @return Response
      */
     public function profile()
     {
@@ -26,5 +29,30 @@ class UserController extends Controller
                 'user' => $user,
             ]
         );
+    }
+
+    /**
+     * @Route("/change_email", name="change_email")
+     * @param Request $request
+     * @return RedirectResponse
+     */
+    public function changeEmail(Request $request)
+    {
+        /** @var User $user */
+        $user = $this->getUser();
+
+        if (empty($request->get('email'))) {
+            $this->addFlash('danger', 'No new emal entered!');
+        } elseif ($request->get('email') == $user->getEmail()) {
+            $this->addFlash('danger', 'New email is same as old one!');
+        } else {
+            $user->setEmail($request->get('email'));
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash('success', 'Email changed!');
+        }
+
+        return $this->redirectToRoute('user_profile');
     }
 }
