2bbd6e46baef4908a50fd15b3461516743d800d0
Aivaras Baranauskas <5412767+aivarasbaranauskas@users.noreply.github.com>
2018-04-10 22:38:25 +0300

    parsers tests

--- tests/Unit/Service/Export/Parser/EventParserTest.php
+++ tests/Unit/Service/Export/Parser/EventParserTest.php
@@ -2,6 +2,11 @@
 
 namespace App\Tests\Unit\Service\Export;
 
+use App\Entity\Event;
+use App\Entity\EventTime;
+use App\Entity\FormConfig;
+use App\Entity\Registration;
+use App\Service\Export\Parser\EventParser;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -9,8 +14,146 @@ use PHPUnit\Framework\TestCase;
  */
 class EventParserTest extends TestCase
 {
-    public function testExport()
+    /**
+     * @return array
+     */
+    public function getTestExportData(): array
     {
-        //TODO: add test
+        $cases = [];
+
+        //case #0
+        $cases[] = [
+            new Event(),
+            [
+                ['No times set for event'],
+            ],
+        ];
+
+        //case #1
+        $cases[] = [
+            new EventTime(),
+            [],
+            true,
+        ];
+
+        //case #2
+        $event = new Event();
+        $this->addEventTime($event, '2018-01-10 10:00', []);
+        $this->addEventTime($event, '2018-01-10 12:00', []);
+        $event->setTitle('Event Title');
+
+        $cases[] = [
+            $event,
+            [
+                [],
+                [
+                    'Event',
+                    'Event Title',
+                    '2018-01-10 10:00',
+                ],
+                [
+                    'No registrations in event',
+                ],
+                [],
+                [
+                    'Event',
+                    'Event Title',
+                    '2018-01-10 12:00',
+                ],
+                [
+                    'No registrations in event',
+                ],
+            ],
+        ];
+
+        //case #3
+        $event = new Event();
+        $this->addEventTime(
+            $event,
+            '2018-01-10 10:00',
+            [
+                [
+                    'created' => '2017-12-20 12:24',
+                    'data'    => [
+                        'name' => 'Aaa',
+                        'age'  => '12',
+                    ],
+                ],
+            ]
+        );
+        $event->setTitle('Event Title');
+        $config = new FormConfig();
+        $config->setConfig(json_encode([
+            [
+                'type'  => 'text',
+                'label' => 'Name',
+                'name'  => 'name',
+            ],
+            [
+                'type'  => 'number',
+                'label' => 'Age',
+                'name'  => 'age',
+            ],
+        ]));
+        $event->setFormConfig($config);
+
+        $cases[] = [
+            $event,
+            [
+                [],
+                [
+                    'Event',
+                    'Event Title',
+                    '2018-01-10 10:00',
+                ],
+                [
+                    'Registration Date',
+                    'Name',
+                    'Age',
+                ],
+                [
+                    '2017-12-20 12:24',
+                    'Aaa',
+                    '12',
+                ],
+            ],
+        ];
+
+        return $cases;
+    }
+
+    /**
+     * @dataProvider getTestExportData
+     * @param Event $object
+     * @param array $expected
+     * @param bool  $expectedException
+     */
+    public function testExport($object, array $expected, $expectedException = false)
+    {
+        try {
+            $result = EventParser::parse($object);
+        } catch (\Exception $e) {
+            $this->assertSame(true, $expectedException, $e->getMessage());
+            $result = [];
+        }
+
+        $this->assertEquals($expected, $result);
+    }
+
+    private function addEventTime(Event $event, string $startTime, array $registrations): void
+    {
+        $eventTime = new EventTime();
+        $eventTime->setStartTime(new \DateTime($startTime));
+
+        foreach ($registrations as $registration) {
+            $reg = new Registration();
+            $reg->setCreated(new \DateTime($registration['created']));
+            $reg->setData($registration['data']);
+            $reg->setEventTime($eventTime);
+            $eventTime->getRegistrations()->add($reg);
+        }
+
+        $eventTime->setEvent($event);
+        $event->getTimes()->add($eventTime);
     }
 }
--- tests/Unit/Service/Export/Parser/MultiEventParserTest.php
+++ tests/Unit/Service/Export/Parser/MultiEventParserTest.php
@@ -2,6 +2,13 @@
 
 namespace App\Tests\Unit\Service\Export;
 
+use App\Entity\Category;
+use App\Entity\FormConfig;
+use App\Entity\MultiEvent;
+use App\Entity\Registration;
+use App\Entity\Workshop;
+use App\Entity\WorkshopTime;
+use App\Service\Export\Parser\MultiEventParser;
 use PHPUnit\Framework\TestCase;
 
 /**
@@ -9,8 +16,247 @@ use PHPUnit\Framework\TestCase;
  */
 class MultiEventParserTest extends TestCase
 {
-    public function testExport()
+    /**
+     * @return array
+     */
+    public function getTestExportData(): array
     {
-        //TODO: add test
+        $cases = [];
+
+        //case #0
+        $event = new MultiEvent();
+        $event->setTitle('Multi Event Title');
+        $cases[] = [
+            $event,
+            [
+                [
+                    'MultiEvent',
+                    'Multi Event Title',
+                ],
+            ],
+        ];
+
+        //case #1
+        $cases[] = [
+            new Category(),
+            [],
+            true,
+        ];
+
+        //case #2
+        $event = new MultiEvent();
+        $event->setTitle('Multi Event Title');
+        $category = new Category();
+        $category->setTitle('Category Title');
+        $event->getCategories()->add($category);
+        $category->setEvent($event);
+        $category = new Category();
+        $category->setTitle('Category2 Title');
+        $event->getCategories()->add($category);
+        $category->setEvent($event);
+        $cases[] = [
+            $event,
+            [
+                [
+                    'MultiEvent',
+                    'Multi Event Title',
+                ],
+                [],
+                [
+                    'Category',
+                    'Category Title',
+                ],
+                ['No workshops in category'],
+                [],
+                [
+                    'Category',
+                    'Category2 Title',
+                ],
+                ['No workshops in category'],
+            ],
+        ];
+
+        //case #3
+        $event = new MultiEvent();
+        $event->setTitle('Multi Event Title');
+        $category = new Category();
+        $category->setTitle('Category Title');
+        $event->getCategories()->add($category);
+        $category->setEvent($event);
+        $workshop = new Workshop();
+        $workshop->setCategory($category);
+        $category->getWorkshops()->add($workshop);
+        $workshop = new Workshop();
+        $workshop->setCategory($category);
+        $category->getWorkshops()->add($workshop);
+        $cases[] = [
+            $event,
+            [
+                [
+                    'MultiEvent',
+                    'Multi Event Title',
+                ],
+                [],
+                [
+                    'Category',
+                    'Category Title',
+                ],
+                ['No times set for workshop'],
+                ['No times set for workshop'],
+            ],
+        ];
+
+        //case #4
+        $event = new MultiEvent();
+        $event->setTitle('Multi Event Title');
+        $category = new Category();
+        $category->setTitle('Category Title');
+        $event->getCategories()->add($category);
+        $category->setEvent($event);
+        $workshop = new Workshop();
+        $workshop->setTitle('Workshop Title');
+        $this->addWorkshopTime($workshop, '2018-01-01 12:00', []);
+        $this->addWorkshopTime($workshop, '2018-01-01 15:00', []);
+        $workshop->setCategory($category);
+        $category->getWorkshops()->add($workshop);
+        $cases[] = [
+            $event,
+            [
+                [
+                    'MultiEvent',
+                    'Multi Event Title',
+                ],
+                [],
+                [
+                    'Category',
+                    'Category Title',
+                ],
+                [],
+                [
+                    'Workshop',
+                    'Workshop Title',
+                    '2018-01-01 12:00',
+                ],
+                ['No registrations in workshop'],
+                [],
+                [
+                    'Workshop',
+                    'Workshop Title',
+                    '2018-01-01 15:00',
+                ],
+                ['No registrations in workshop'],
+            ],
+        ];
+
+        //case #5
+        $event = new MultiEvent();
+        $event->setTitle('Multi Event Title');
+        $category = new Category();
+        $category->setTitle('Category Title');
+        $event->getCategories()->add($category);
+        $category->setEvent($event);
+        $workshop = new Workshop();
+        $workshop->setTitle('Workshop Title');
+        $this->addWorkshopTime(
+            $workshop,
+            '2018-01-01 12:00',
+            [
+                [
+                    'created' => '2017-12-20 12:34',
+                    'data'    => [
+                        'name' => 'Aaa',
+                        'age'  => '12',
+                    ],
+                ],
+            ]
+        );
+        $config = new FormConfig();
+        $config->setConfig(json_encode([
+            [
+                'type'  => 'text',
+                'label' => 'Name',
+                'name'  => 'name',
+            ],
+            [
+                'type'  => 'number',
+                'label' => 'Age',
+                'name'  => 'age',
+            ],
+        ]));
+        $workshop->setFormConfig($config);
+        $workshop->setCategory($category);
+        $category->getWorkshops()->add($workshop);
+        $cases[] = [
+            $event,
+            [
+                [
+                    'MultiEvent',
+                    'Multi Event Title',
+                ],
+                [],
+                [
+                    'Category',
+                    'Category Title',
+                ],
+                [],
+                [
+                    'Workshop',
+                    'Workshop Title',
+                    '2018-01-01 12:00',
+                ],
+                [
+                    'Registration Date',
+                    'Name',
+                    'Age',
+                ],
+                [
+                    '2017-12-20 12:34',
+                    'Aaa',
+                    '12',
+                ],
+            ],
+        ];
+
+        return $cases;
+    }
+
+    /**
+     * @dataProvider getTestExportData
+     * @param MultiEvent $object
+     * @param array      $expected
+     * @param bool       $expectedException
+     */
+    public function testExport($object, array $expected, $expectedException = false)
+    {
+        try {
+            $result = MultiEventParser::parse($object);
+        } catch (\Exception $e) {
+            $this->assertSame(true, $expectedException, $e->getMessage());
+            $result = [];
+        }
+
+        $this->assertEquals($expected, $result);
+    }
+
+    /**
+     * @param Workshop $workshop
+     * @param string   $startTime
+     * @param array    $registrations
+     */
+    private function addWorkshopTime(Workshop $workshop, string $startTime, array $registrations): void
+    {
+        $workshopTime = new WorkshopTime();
+        $workshopTime->setStartTime(new \DateTime($startTime));
+
+        foreach ($registrations as $registration) {
+            $reg = new Registration();
+            $reg->setCreated(new \DateTime($registration['created']));
+            $reg->setData($registration['data']);
+            $reg->setWorkshopTime($workshopTime);
+            $workshopTime->getRegistrations()->add($reg);
+        }
+
+        $workshopTime->setWorkshop($workshop);
+        $workshop->getTimes()->add($workshopTime);
     }
 }
